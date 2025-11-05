4310725b97ae029f4357bec38a5c7968f5e3ab35
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-05 15:21:55 -0800

    Added event search for movies on landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,22 +2,44 @@ import React from 'react'
 import '../index.css';
 import LandingImg from '../assets/landing-page-img.webp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react'
 
 export default function Landing() {
+    const [movieName, setMovieName] = useState("Search by Movie Name")
+
+    const movieSearchChange = event => {
+        setMovieName(event.target.value)
+    }
+
+    const enterKeySearch = event => {
+        if (event.key === 'Enter') {
+            console.log("enter key pressed")
+        }
+
+        if (movieName.length <= 0) {
+            alert("Please search a movie name that is not blank.")
+        }
+    }
+
+    function clickSearch() {
+        console.log("clicked")
+    }
+
     return (
         <section id="landing-page">
             <div className="landing-page__wrapper">
                 <div className="landing-page__wrapper--intro">
                     <h1 className="red">The best movie database</h1>
-                    <h3>Find more information about your favorite <span class="red">movies!</span></h3>
+                    <h3>Find more information about your favorite <span className="red">movies!</span></h3>
                 </div>
                 <div className="landing-page__input--wrapper">
-                    <input className="movie-search" id="movie-name" placeholder="Search by Movie Name" type="text" />
-                    <button className="movie-search-button" id="movie-name">
+                    <input className="movie-search" id="movie-name"
+                        value={movieName} onChange={movieSearchChange} onKeyDown={enterKeySearch} />
+                    <button className="movie-search-button" id="movie-name" onClick={clickSearch}>
                         <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
                     </button>
                 </div>
-                <img src={LandingImg} alt="Movie Watching"/>
+                <img src={LandingImg} alt="Movie Watching" />
             </div>
         </section>
     )

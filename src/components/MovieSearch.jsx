1cc2b9bfe476720e97ceaea2b694575282761811
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-05 17:19:17 -0800

    changed from class to className

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 export default function MovieSearch() {
     return (
         <>
-            <div class="navbar__input--wrapper">
+            <div className="navbar__input--wrapper">
                 <h3>Search for your movie</h3>
-                <input class="movie-search" placeholder="Search by Movie Name" type="text" />
+                <input className="movie-search" placeholder="Search by Movie Name" type="text" />
             </div>
 
             <section id="search">
-                <div class="search-filter">
-                    <h2 class="search-filter__info">
+                <div className="search-filter">
+                    <h2 className="search-filter__info">
                         <span>Search results for</span>
-                        <span class="red" id="movie-name-filter">""</span>
+                        <span className="red" id="movie-name-filter">""</span>
                     </h2>
-                    <div class="year-filter">
+                    <div className="year-filter">
                         <h2>
                             <span>Search by Year</span>
                         </h2>
-                        <input type="number" min="1878" max="2025" class="year-filter__input" placeholder="e.g. 2025" />
+                        <input type="number" min="1878" max="2025" className="year-filter__input" placeholder="e.g. 2025" />
                     </div>
                 </div>
 
-                <div class="movie-container">
-                    <div class="movie-container__row">
-                        <div class="movie-list">
-                            <div class="movie-list__item">
-                                <div class="movie-card">
-                                    <div class="movie-card__container">
+                <div className="movie-container">
+                    <div className="movie-container__row">
+                        <div className="movie-list">
+                            <div className="movie-list__item">
+                                <div className="movie-card">
+                                    <div className="movie-card__container">
                                         <img id="movie-poster" src="" alt="Movie Poster" />
                                             <p><b>Movie name:</b> movie name</p>
                                             <p><b>Year:</b> movie year</p>
@@ -40,8 +40,6 @@ export default function MovieSearch() {
                     </div>
                 </div>
             </section>
-
         </>
-
     )
 }

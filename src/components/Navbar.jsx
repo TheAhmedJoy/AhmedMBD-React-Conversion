7b5bab910f75492552820cbe16c6301bef344441
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-05 16:07:18 -0800

    Added correct page routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,11 @@ import NavLogo from '../assets/AhmedMDB-Logo.png'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+
+    function contactAlert() {
+        alert("Not implemented")
+    }
+
     return (
         <header id="home">
             <nav>
@@ -12,17 +17,14 @@ export default function Navbar() {
                 </figure>
                 <ul className="nav__link--list">
                     <li className="nav__link--list-item">
-                        {/* <Link to="/" className="nav__link--anchor link__hover-effect link__hover-effect--black">Home</Link> */}
-                        <a href="/" className="nav__link--anchor link__hover-effect link__hover-effect--black">Home</a>
+                        <Link to="/" className="nav__link--anchor link__hover-effect link__hover-effect--black">Home</Link>
                     </li>
                     <li className="nav__link--list-item">
-                        {/* <Link to="/MoviesPage" className="nav__link--anchor link__hover-effect link__hover-effect--black">Find your Movie</Link> */}
-                        <a href="movie-page.html" className="nav__link--anchor link__hover-effect link__hover-effect--black">Find your
-                            movie</a>
+                        <Link to="/MoviesPage" className="nav__link--anchor link__hover-effect link__hover-effect--black">Find your Movie</Link>
                     </li>
                     <li className="nav__link--list-item click">
-                        <a href="index.html" className="nav__link--anchor">
-                            <button className="nav__link--contact-button">Contact</button>
+                        <a href={null} className="nav__link--anchor">
+                            <button className="nav__link--contact-button" onClick={contactAlert}>Contact</button>
                         </a>
                     </li>
                 </ul>

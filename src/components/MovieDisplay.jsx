6e9da23ce1d2b0a40b32094c183dfb6a0fe9044a
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-12 17:07:52 -0800

    added on click handler to move to movie info display page for a given movie

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import '../movie-page-styles.css'
 import MoviePosterMissing from '../assets/movie-poster-missing.jpg'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function MovieDisplay({ movieInfo }) {
+  let navigate = useNavigate()
+
+  function movieInfoPage(movieID) {
+    navigate(`/MovieInfoPage/${movieID}`)
+  }
 
   return (
     <>
@@ -11,7 +18,8 @@ export default function MovieDisplay({ movieInfo }) {
           <div className="movie-list__item">
             <div className="movie-card">
               <div className="movie-card__container">
-                <img id="movie-poster" src={(movieInfo.Poster !== "N/A" ? movieInfo.Poster : MoviePosterMissing)} alt="Movie Poster" />
+                <img id="movie-poster" src={(movieInfo.Poster !== "N/A" ? movieInfo.Poster : MoviePosterMissing)} alt="Movie Poster"
+                  onClick={() => movieInfoPage(movieInfo.imdbID)} />
                 <p><b>Movie name: </b>{movieInfo.Title}</p>
                 <p><b>Year:</b> {movieInfo.Year}</p>
                 <p><b>IMDb:</b> <a id="movie-imbd" href={`https://www.imdb.com/title/${movieInfo.imdbID}`}

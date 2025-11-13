81636cb0fd62e658ad55e5506fe7f26fb80fbf65
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-12 17:06:51 -0800

    Added route for movie info page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MoviesPage from './pages/MoviesPage';
+import MovieInfoPage from './pages/MovieInfoPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' exact Component={HomePage} />
           <Route path='/MoviesPage' exact Component={MoviesPage} />
+          <Route path='/MovieInfoPage/:movieID' exact Component={MovieInfoPage} />
         </Routes>
       </div>
     </Router>

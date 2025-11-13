a5633f602218f03030b33711129fc48b7efbbf28
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-11-12 17:08:43 -0800

    removed commented import

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '../movie-page-styles.css'
 import { useLocation } from 'react-router-dom'
 import MovieSearch from '../components/MovieSearch'
-// import MovieDisplay from '../components/MovieDisplay'
 
 export default function MoviesPage() {
     const {movieName} = useLocation().state ?? "Search by Movie Name"
